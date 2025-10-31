e6f69fbac3d53064e8a503d185c0fd69f44de4bb
KabriAcid <kabriacid01@gmail.com>
2025-10-31 02:14:43 +0100

    feat: add favicon to customer pages and products; update HTML head sections

--- frontend/customer/profile.php
+++ frontend/customer/profile.php
@@ -8,7 +8,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>My Profile - Aquaflow</title>
-    <link rel="stylesheet" href="../css/tailwind.css">
+    <!-- Favicon -->
+    <link rel="icon" type="image/png" href="../../favicon.png">
+
     <link rel="stylesheet" href="../css/tailwind.css">
     <link rel="stylesheet" href="../css/style.css">
 
