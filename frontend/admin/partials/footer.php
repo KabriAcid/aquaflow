53fe71a149d48aed5346f5ab3b291a5d44d0d2f0
KabriAcid <kabriacid01@gmail.com>
2025-11-06 13:47:52 +0100

    fix: Add missing newline at end of manage-users.php; update footer styles for better visibility

--- frontend/admin/partials/footer.php
+++ frontend/admin/partials/footer.php
@@ -1,5 +1,5 @@
 <footer>
-    <div class="text-center text-xs">
+    <div class="text-center text-xs mt-8 text-gray-300">
         @copyright 2024 Aquaflow. All rights reserved.
     </div>
 </footer>
